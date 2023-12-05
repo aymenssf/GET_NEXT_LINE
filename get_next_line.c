88e60dfee3697f59b8cb3e36d4dc03a48315f650
aymenssf <aymen.assaf@edu.uca.ma>
2023-12-05 20:18:51 +0100

    get next line implementation

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,110 @@
 /*   By: aassaf <aassaf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 09:32:36 by aassaf            #+#    #+#             */
-/*   Updated: 2023/12/01 09:32:37 by aassaf           ###   ########.fr       */
+/*   Updated: 2023/12/05 19:49:14 by aassaf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+int found_newline(t_list *lst)
+{
+        int i;
+        t_list *tmp;
+
+        if(lst == NULL)
+                return (0);
+        tmp = ft_lstlast(lst);
+        i = 0;
+        while(tmp->content[i])
+        {
+                if (tmp->content[i] == '\n')
+                        return (1);
+                i++;                
+        }
+        return (0);
+}
+void add_to_lst(t_list **lst, char *buff, int nb_read)
+{
+        int i;
+        t_list *tmp;
+        t_list *new_node;
+
+        new_node = malloc(sizeof(t_list));
+        if(!new_node)
+                return ;
+        new_node->next = NULL;
+        new_node->content = malloc(sizeof(char) * (nb_read + 1));
+        if(!new_node->content)
+                return ;
+        i = 0;
+        while(buff[i] && i < nb_read)
+        {
+                new_node->content[i] = buff[i];
+                i++;
+        }
+        if(*lst == NULL)
+        {
+                *lst = new_node;
+                return ;
+        }
+        tmp = ft_lstlast(*lst);
+        tmp->next = new_node;
+}
+void        read_and_store(int fd, t_list **lst)
+{
+        int nb_read;
+        char *buff;
+        
+        nb_read = 1;
+        while(!found_newline(*lst) && nb_read != 0)
+        {
+                buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+                if(!buff)
+                        return ;
+                nb_read = (int)read(fd, buff, BUFFER_SIZE);
+                if((*lst == NULL && nb_read == 0) || nb_read == -1)
+                {
+                        free (buff);
+                        return ;
+                }
+                buff[nb_read] = '\0';
+                add_to_lst(lst, buff, nb_read);
+                free(buff);  
+        }
+}
+
+char *get_next_line(int fd)
+{
+        static  t_list *lst = NULL;
+        char    *line;
+        if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &line, 0) < 0)
+                return (NULL);
+        line = NULL;
+        read_and_store(fd, &lst);
+        if(!lst)
+                return (NULL);
+        copy_line(lst, &line);
+        clear_lst(&lst);
+        if(line[0] == '\0')
+        {
+                free_all(lst);
+                lst = NULL;
+                free(line);
+                return (NULL);
+        }
+        return(line);
+}
+
+/* int main()
+{
+        int fd;
+        char *line;
+        fd = open("test.txt", O_RDONLY);
+        while((line = get_next_line(fd)))
+        {
+                printf("%s\n", line);
+                free(line);
+        }
+        return (0);
+} */
